05ed98cfde253e874aff274339b79c75594c31cf
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-25 20:24:37 +0530

    Placeholder text replacement

--- components/simple_footer_component_1721919268.js
+++ components/simple_footer_component_1721919268.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1721919268", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            BookBuzz: Your Online Bookstore
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Discover your next great read at BookBuzz. Browse our extensive collection of books across genres, read reviews, and enjoy seamless online ordering and delivery. Happy reading!
                     </div>
                 </div>
             </div>
